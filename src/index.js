1c414928da3d154e70675ae5ef3c8c3c5356a16b
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-12 15:51:24 -0400

    first hello world on react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-// Create a new component => produce some HTML
+//please get react on my modules
+import React from 'react'; //create and manage components
+import ReactDOM from 'react-dom'; //to manipulate DOM
 
-const App = function () {
-    return <div> hi! </div>;
+// Create a new component => produce some HTML
+const App = () => {
+    return <div> hello, world! </div>;
 };
 
 // take the components generated HTML and put it on the page (in the DOM)
+// ReactDOM.render(App); //return an error cuz it was passing a class
+ReactDOM.render(<App />, document.querySelector('.container'));
