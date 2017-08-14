226639302b425bf7d39f1c449c12912ad39cb45a
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-14 15:37:07 -0400

    refactored to a class based component

---
+++ src/components/search_bar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; //for jsx
+
+//functional component
+// const SearchBar = () => {
+//   return <input />
+// };
+
+//class based component
+//class SearchBar extends React.Component {
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@
 import React from 'react'; //create and manage components
 import ReactDOM from 'react-dom'; //to manipulate DOM
 
+//components
+import SearchBar from './components/search_bar';
+
+//Api key lives here
+const API_KEY = config.MY_KEY;
+
 // Create a new component => produce some HTML
 const App = () => {
-    return <div> hello, world! </div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 };
 
 // take the components generated HTML and put it on the page (in the DOM)
