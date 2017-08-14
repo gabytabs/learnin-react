e2111e5ac6d0fb1f019736b4d9c41581635bdacc
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-14 16:22:22 -0400

    init states in react

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,9 +8,29 @@ import React, { Component } from 'react'; //for jsx
 //class based component
 //class SearchBar extends React.Component {
 class SearchBar extends Component {
-    render() {
-        return <input />;
+
+    //constructor is to initialize variables and states. It is used to set things up
+    constructor(props) {
+        //Component has a constructor function also.
+        //When defining method that is already defined on the parent Class which is "Component"
+        //The parent method on the parent class(Component) can be called by super();
+        super(props);
+
+        //To initialize state, make an object and assign it to "this.state"
+        this.state = { term: '' };
     }
+
+    render() {
+        //onChange is a react property
+        return <input
+                // onChange={this.onInputChange}
+                onChange={event => console.log(event.target.value)} />;
+    }
+
+    // onInputChange(event){
+    //     //event.target.value is the value you input in the Input DOM
+    //      console.log(event.target.value)
+    // }
 }
 
 export default SearchBar;
